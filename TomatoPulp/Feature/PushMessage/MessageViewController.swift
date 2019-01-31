c566a9c616fd428917d884174f4bcefa26ab23de
SWING1993 <songguohua10446627@gmail.com>
2019-01-31 09:17:35 +0800

    update

--- TomatoPulp/Feature/PushMessage/MessageViewController.swift
+++ TomatoPulp/Feature/PushMessage/MessageViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import ReactiveCocoa
+import ReactiveSwift
+import Material
 
 class MessageViewController: UIViewController {
 
@@ -15,16 +18,6 @@ class MessageViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
