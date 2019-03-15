b8ce7d0f354689fe90d6993a3ae02c76bc3a2533
SWING1993 <songguohua10446627@gmail.com>
2019-03-15 17:48:42 +0800

    update

---
+++ TomatoPulp/Feature/Status/Model/SWStatusModel.swift
@@ -0,0 +1,26 @@
+//
+//  SWStatusModel.swift
+//  TomatoPulp
+//
+//  Created by 宋国华 on 2019/3/15.
+//  Copyright © 2019 songguohua. All rights reserved.
+//
+
+import UIKit
+import HandyJSON
+
+class SWStatusModel: HandyJSON {
+    var id : Int64 = 0
+    var uid: Int64 = 0
+    // 状态类型 0 = 图文  1 = 视频
+    var type : Int = 0
+    var nickname : String = ""
+    var avatarUrl : String = ""
+    var content: String = ""
+    var imageUrls: String = ""
+    var vedioUrl: String = ""
+    var created: Int64 = 0
+    var fromDevice: String = ""
+
+    required init() {}
+}
---
+++ TomatoPulp/Feature/Status/ViewModel/SWStatusViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SWStatusViewModel.swift
+//  TomatoPulp
+//
+//  Created by 宋国华 on 2019/3/15.
+//  Copyright © 2019 songguohua. All rights reserved.
+//
+
+import UIKit
+
+class SWStatusViewModel: NSObject {
+
+}
